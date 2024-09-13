c0113944a384d49213b65f739f96f51587ee2902
Guru <guru@Gurus-MacBook-Pro.local>
2024-09-13 10:42:08 +0530

    Added conditional statement example

---
+++ conditional-statments.js
@@ -0,0 +1,10 @@
+//Conditional Statement
+let name = "virat"
+
+if(name !== "virat"){
+  console.log("Happy Birthday " + name)
+} else if(name === "dhoni") {
+  console.log("Happy Birthday Dhoni")
+} else {
+  console.log("Happy Birthday Somebody Else")
+}
